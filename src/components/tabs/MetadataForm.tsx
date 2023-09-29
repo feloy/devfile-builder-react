e4f1ff7228a9106bf6bfcd99e86bba6cf060c2d7
Philippe Martin <phmartin@redhat.com>
2023-09-29 11:49:32 +0200

    validation + placeholders

--- src/components/tabs/MetadataForm.tsx
+++ src/components/tabs/MetadataForm.tsx
@@ -17,11 +17,25 @@ function MetadataForm(props: props) {
         props.onApply(metadata);
     }    
 
-    return <>
+    const [versionErrorMsg, setVersionErrorMsg] = useState('');
+    const semverPattern = `^([0-9]+)\\.([0-9]+)\\.([0-9]+)(\\-[0-9a-z-]+(\\.[0-9a-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$`;
+    const semverPatternRegex = new RegExp(semverPattern);
+
+    const onVersionChange = (v: string) => {
+        if (semverPatternRegex.test(v)) {
+            setVersionErrorMsg('');
+        } else {
+            setVersionErrorMsg('Examples: 1.0.4, 1.4.7-alpha1')
+        }
+        setMetadata({...metadata, version: v});
+    }
+
+return <>
         <Grid container spacing={2}>
             <Grid item xs={6}>
                 <TextField
                     label="Name" variant="outlined" fullWidth
+                    placeholder="Unique name to identify the devfile"
                     value={metadata.name}
                     onChange={(e) => setMetadata({...metadata, name: e.target.value})}
                 />
@@ -29,13 +43,17 @@ function MetadataForm(props: props) {
             <Grid item xs={6}>
                 <TextField
                     label="Version" variant="outlined" fullWidth
+                    placeholder="Version of the devfile, semver-compatible" 
                     value={metadata.version}
-                    onChange={(e) => setMetadata({...metadata, version: e.target.value})}
+                    onChange={(e) => onVersionChange(e.target.value) }
+                    helperText={versionErrorMsg}
+                    error={!!versionErrorMsg}
                     />
             </Grid>
             <Grid item xs={12}>
                 <TextField
                     label="Display Name" variant="outlined" fullWidth
+                    placeholder="Name to display instead of the unique name"
                     value={metadata.displayName}
                     onChange={(e) => setMetadata({...metadata, displayName: e.target.value})}
                     />
@@ -47,16 +65,10 @@ function MetadataForm(props: props) {
                     onChange={(e) => setMetadata({...metadata, description: e.target.value})}
                     />
             </Grid>
-            <Grid item xs={12}>
-                <TextField
-                    label="Display Name" variant="outlined" fullWidth
-                    value={metadata.displayName}
-                    onChange={(e) => setMetadata({...metadata, displayName: e.target.value})}
-                    />
-            </Grid>
             <Grid item xs={6}>
                 <TextField
                     label="Tags" variant="outlined" fullWidth
+                    placeholder="Tags to help find the devfile in a registry" 
                     value={metadata.tags}
                     onChange={(e) => setMetadata({...metadata, tags: e.target.value})}
                     />
@@ -64,21 +76,26 @@ function MetadataForm(props: props) {
             <Grid item xs={6}>
                 <TextField
                     label="Architectures" variant="outlined" fullWidth
+                    placeholder="Ex: amd64,arm64,ppc64le,s390x" 
                     value={metadata.architectures}
                     onChange={(e) => setMetadata({...metadata, architectures: e.target.value})}
                     />
             </Grid>
-            <Grid item xs={7}>
+            <Grid item xs={6}>
                 <TextField
                     label="Icon" variant="outlined" fullWidth
+                    placeholder="Can be a URI or a relative path in the project" 
                     value={metadata.icon}
                     onChange={(e) => setMetadata({...metadata, icon: e.target.value})}
                     />
+            </Grid>
+            <Grid item xs={1}>
                 {/* TODO(feloy): display icon */}
             </Grid>
             <Grid item xs={5}>
                 <TextField
                     label="Global Memory Limit" variant="outlined" fullWidth
+                    placeholder="Informative limit of memory used. Ex: 1Gi"
                     value={metadata.globalMemoryLimit}
                     onChange={(e) => setMetadata({...metadata, globalMemoryLimit: e.target.value})}
                     />
@@ -86,6 +103,7 @@ function MetadataForm(props: props) {
             <Grid item xs={6}>
                 <TextField
                     label="Project Type" variant="outlined" fullWidth
+                    placeholder="Ex: Framework of the project"
                     value={metadata.projectType}
                     onChange={(e) => setMetadata({...metadata, projectType: e.target.value})}
                     />
@@ -93,6 +111,7 @@ function MetadataForm(props: props) {
             <Grid item xs={6}>
                 <TextField
                     label="Language" variant="outlined" fullWidth
+                    placeholder="Language of the project"
                     value={metadata.language}
                     onChange={(e) => setMetadata({...metadata, language: e.target.value})}
                     />
@@ -100,6 +119,7 @@ function MetadataForm(props: props) {
             <Grid item xs={6}>
                 <TextField
                     label="Website" variant="outlined" fullWidth
+                    placeholder="Official website of the devfile"
                     value={metadata.website}
                     onChange={(e) => setMetadata({...metadata, website: e.target.value})}
                     />
@@ -107,6 +127,7 @@ function MetadataForm(props: props) {
             <Grid item xs={6}>
                 <TextField
                     label="Provider" variant="outlined" fullWidth
+                    placeholder="Information about the provider of the devfile"
                     value={metadata.provider}
                     onChange={(e) => setMetadata({...metadata, provider: e.target.value})}
                     />
@@ -114,6 +135,7 @@ function MetadataForm(props: props) {
             <Grid item xs={12}>
                 <TextField
                     label="Support URL" variant="outlined" fullWidth
+                    placeholder="Link to a page providing support information"
                     value={metadata.supportUrl}
                     onChange={(e) => setMetadata({...metadata, supportUrl: e.target.value})}
                     />
